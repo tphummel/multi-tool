2c741904179e026cb40dcf063b574b11f189a751
Tom Hummel <tohu@hey.com>
2023-05-17 14:25:42 -0700

    remove trailing slashes

--- kits/more-tools.sh
+++ kits/more-tools.sh
@@ -1,8 +1,8 @@
-curl -OL https://github.com/fullstorydev/grpcurl/releases/download/v1.8.7/grpcurl_1.8.7_linux_arm64.tar.gz \
-tar -xzf grpcurl_1.8.7_linux_arm64.tar.gz \
-mv grpcurl /usr/local/bin/ \
-rm grpcurl_1.8.7_linux_arm64.tar.gz \
+curl -OL https://github.com/fullstorydev/grpcurl/releases/download/v1.8.7/grpcurl_1.8.7_linux_arm64.tar.gz
+tar -xzf grpcurl_1.8.7_linux_arm64.tar.gz
+mv grpcurl /usr/local/bin/
+rm grpcurl_1.8.7_linux_arm64.tar.gz
 
-curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.5.9/aws-iam-authenticator_0.5.9_linux_arm64 \
-chmod +x ./aws-iam-authenticator \
+curl -Lo aws-iam-authenticator https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.5.9/aws-iam-authenticator_0.5.9_linux_arm64
+chmod +x ./aws-iam-authenticator
 mv aws-iam-authenticator /usr/local/bin/
